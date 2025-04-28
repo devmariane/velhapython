8d61781b3e8ea0a4d45f3d73c8aec696188b6870
Mariane Fatima <fmari6979@gmail.com>
2025-04-28 16:47:25 -0300

    logica do jogo

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 from tabuleiro import JogoDaVelha
 jogo = JogoDaVelha()
-
-jogo.exibirTabuleiro()
-posicao = input(f"Vez de {jogo.ProximoJogador()}: ")
+jogo.IniciarJogo()
--- tabuleiro.py
+++ tabuleiro.py
@@ -5,9 +5,8 @@ class JogoDaVelha:
          '4': ' ', '5': ' ', '6': ' ',
          '1': ' ', '2': ' ', '3': ' '
          }
-       self.rodada = 1
 
-  def exibirTabuleiro(self):
+  def ExibirTabuleiro(self):
     print("┌───┬───┬───┐")
     print(f"│ {self.tabuleiro['7']} │ {self.tabuleiro['8']} │ {self.tabuleiro['9']} │")
     print("├───┼───┼───┤")
@@ -16,33 +15,57 @@ class JogoDaVelha:
     print(f"│ {self.tabuleiro['1']} │ {self.tabuleiro['2']} │ {self.tabuleiro['3']} │")
     print("└───┴───┴───┘")
   
-  def Rodada(self):
-     while not self.Vencedor or not self.Velha():
-        rodada += 1
-        return rodada
-     
-  def ProximoJogador(self):
-    if self.Rodada()%2==0:
+  def IniciarJogo(self):
+     jogardnv = True
+     while jogardnv:
+      while not self.Vencedor() or not self.Velha():
+          rodada = 0
+          self.ExibirTabuleiro()
+          
+          posicao = input(f"Vez de {self.ProximoJogador(rodada)}: ")
+          self.ValidarPosicao(posicao, rodada)
+          return rodada
+      if self.Velha():
+        print("fim de jogo: deu velha!")
+        novojogo = input("Jogar novamente?(S/N)")
+        if novojogo == 'N' or novojogo == 'n':
+          jogardnv = False
+      else:
+        print(f"Fim de jogo vencedor: {self.ProximoJogador}")
+        novojogo = input("Jogar novamente?(S/N)")
+        if novojogo == 'N' or novojogo == 'n':
+          jogardnv = False
+  
+  def ProximoJogador(self, rodada):
+    if rodada%2==0:
       return "x"
     else:
       return "O"
   def Vencedor(self):
+    print("Chama Vencedor")
     lines = [
-    [7, 8, 9],
-    [4, 5, 6],
-    [1, 2, 3],
-    [7, 4, 1],
-    [8, 5, 2],
-    [9, 6, 3],
-    [7, 5, 3],
-    [1, 5, 9],
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['7', '4', '1'],
+    ['8', '5', '2'],
+    ['9', '6', '3'],
+    ['7', '5', '3'],
+    ['1', '5', '9'],
     ]
     for i, (a, b, c) in enumerate(lines):
-      if self.tabuleiro['a'] and self.tabuleiro['b'] == self.tabuleiro['c']:
+      if self.tabuleiro[a] and self.tabuleiro[b] == self.tabuleiro[c]:
         return True
-        
      
   def Velha(self):
+    print("Chama Velha")
     for i in self.tabuleiro:
       if i == ' ':
          return True
+
+  def ValidarPosicao(self, posicao, rodada):
+    if self.tabuleiro[posicao] == ' ':
+      self.tabuleiro[posicao] == self.ProximoJogador
+      rodada += 1
+    else:
+      print("Posicao invalida, jogue novamente")
