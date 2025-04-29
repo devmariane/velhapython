d12bab727372497a2a682d349138a4b061e50211
Mariane Fatima <fmari6979@gmail.com>
2025-04-29 12:33:26 -0300

    edicao

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,9 +1,9 @@
 class JogoDaVelha:
   def __init__(self):
        self.tabuleiro = {
-         '7': ' ', '8': ' ', '9': ' ',
-         '4': ' ', '5': ' ', '6': ' ',
-         '1': ' ', '2': ' ', '3': ' '
+         7: ' ', 8: ' ', 9: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         1: ' ', 2: ' ', 3: ' '
          }
        self.rodada = 0
 
@@ -15,28 +15,31 @@ class JogoDaVelha:
 
   def ExibirTabuleiro(self):
     print("┌───┬───┬───┐")
-    print(f"│ {self.tabuleiro['7']} │ {self.tabuleiro['8']} │ {self.tabuleiro['9']} │")
+    print(f"│ {self.tabuleiro[7]} │ {self.tabuleiro[8]} │ {self.tabuleiro[9]} │")
     print("├───┼───┼───┤")
-    print(f"│ {self.tabuleiro['4']} │ {self.tabuleiro['5']} │ {self.tabuleiro['6']} │")
+    print(f"│ {self.tabuleiro[4]} │ {self.tabuleiro[5]} │ {self.tabuleiro[6]} │")
     print("├───┼───┼───┤")
-    print(f"│ {self.tabuleiro['1']} │ {self.tabuleiro['2']} │ {self.tabuleiro['3']} │")
+    print(f"│ {self.tabuleiro[1]} │ {self.tabuleiro[2]} │ {self.tabuleiro[3]} │")
     print("└───┴───┴───┘")
   
   def IniciarJogo(self):
      jogardnv = True
      while jogardnv:
-      while not self.Vencedor() or not self.Velha():
+      while not self.Vencedor() and not self.Velha():
           self.ExibirTabuleiro()
-          
-          posicao = input(f"Vez de {self.ProximoJogador()}: ")
+          posicao = int(input(f"Vez de {self.ProximoJogador()}: "))
           self.ValidarPosicao(posicao)
+
       if self.Velha():
+        self.ExibirTabuleiro()
         print("fim de jogo: deu velha!")
         novojogo = input("Jogar novamente?(S/N)")
         if novojogo == 'N' or novojogo == 'n':
           jogardnv = False
       else:
-        print(f"Fim de jogo vencedor: {self.ProximoJogador}")
+        self.ExibirTabuleiro()
+        vencedor = self.ProximoJogador()
+        print(f"Fim de jogo vencedor: {vencedor}")
         novojogo = input("Jogar novamente?(S/N)")
         if novojogo == 'N' or novojogo == 'n':
           jogardnv = False
@@ -48,26 +51,24 @@ class JogoDaVelha:
     else:
       return "O"
   def Vencedor(self):
-    print("Chama Vencedor")
     lines = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['7', '4', '1'],
-    ['8', '5', '2'],
-    ['9', '6', '3'],
-    ['7', '5', '3'],
-    ['1', '5', '9'],
+    [7, 8, 9],
+    [4, 5, 6],
+    [1, 2, 3],
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3],
+    [7, 5, 3],
+    [1, 5, 9],
     ]
     for i, (a, b, c) in enumerate(lines):
-      if self.tabuleiro[a] == self.tabuleiro[b] and self.tabuleiro[b] == self.tabuleiro[c]:
+      if self.tabuleiro[a] == self.tabuleiro[b] and self.tabuleiro[b] == self.tabuleiro[c] and self.tabuleiro[a] != ' ':
         return True
-     
+    return False
   def Velha(self):
-    print("Chama Velha")
-    for i in self.tabuleiro:
-      if i == ' ':
-         return True
+    for chave in sorted(self.tabuleiro):
+      if self.tabuleiro[chave] == ' ':
+         return False
 
   def ValidarPosicao(self, posicao):
     rodada = self.getRodada()
